dc22494973d81d4489328c3dc821baf3e4f92ca4
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2020-11-23 19:18:21 +0100

    added day and date

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const date = new Date ();
 let month = "";
 
 function main() {
-    startClock();   
+    showClock();   
     //addEventListeners();
 }
 
 //addEventListeners() {}
 
-function startClock() {
+function showClock() {
     updateClock();
     setInterval(updateClock, 1000);
 }
@@ -24,8 +24,17 @@ function updateClock(){
     hours = formatTime(hours);
     minutes = formatTime(minutes);
 
-    const clockDiv = document.getElementById("clock-display");
-    clockDiv.innerText = hours + ":" + minutes;
+    const clockHolder = document.getElementById("clock");
+    clockHolder.innerText = hours + ":" + minutes;
+
+    const weekdayHolder = document.getElementById("weekday-holder");
+    weekdayHolder.innerText = getWeekday(date);
+
+    const dateHolder = document.getElementById("date-holder");
+    dateHolder.innerText = date.getDate();
+
+    const monthHolder = document.getElementById("month-holder");
+    monthHolder.innerText = getMonth(date);
 }
 
 function formatTime (counter) {
@@ -33,4 +42,35 @@ function formatTime (counter) {
         return "0" + counter;
     }
     return counter;
+}
+
+function getWeekday(date) {
+    const weekdayIndex = date.getDay();
+    switch(weekdayIndex) {
+        case 0: return "Sönday";
+        case 1: return "Måndag";
+        case 2: return "Tisdag";
+        case 3: return "Onsdag";
+        case 4: return "Torsdag";
+        case 5: return "Fredag";
+        case 6: return "Lördag";
+    }
+}
+
+function getMonth (date) {
+    const monthIndex = date.getMonth();
+    switch(monthIndex) {
+        case 0: return "januari";
+        case 1: return "februari";
+        case 2: return "mars";
+        case 3: return "april";
+        case 4: return "maj";
+        case 5: return "juni";    
+        case 6: return "juli";
+        case 7: return "augusti";
+        case 8: return "september";
+        case 9: return "oktober";
+        case 10: return "november";
+        case 11: return "december";
+    }
 }
